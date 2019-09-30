c83503e1b6d5a357be5842be8ccbfa0b33b8b44b
Russell Strauss <russellstrauss@gmail.com>
2019-09-30 02:46:04 -0400

    Adding example horizontal bar graph

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -37,6 +37,121 @@ module.exports = function () {
 },{}],4:[function(require,module,exports){
 "use strict";
 
+module.exports = function () {
+  return {
+    settings: {
+      graphicHeight: 400
+    },
+    init: function init() {
+      console.log(d3);
+      this.addBarGraph();
+    },
+    addBarGraph: function addBarGraph() {
+      var data = [{
+        'salesperson': 'Bob',
+        'sales': 33
+      }, {
+        'salesperson': 'Robin',
+        'sales': 12
+      }, {
+        'salesperson': 'Anne',
+        'sales': 41
+      }, {
+        'salesperson': 'Mark',
+        'sales': 16
+      }, {
+        'salesperson': 'Joe',
+        'sales': 59
+      }, {
+        'salesperson': 'Eve',
+        'sales': 38
+      }, {
+        'salesperson': 'Karen',
+        'sales': 21
+      }, {
+        'salesperson': 'Kirsty',
+        'sales': 25
+      }, {
+        'salesperson': 'Chris',
+        'sales': 30
+      }, {
+        'salesperson': 'Lisa',
+        'sales': 47
+      }, {
+        'salesperson': 'Tom',
+        'sales': 5
+      }, {
+        'salesperson': 'Stacy',
+        'sales': 20
+      }, {
+        'salesperson': 'Charles',
+        'sales': 13
+      }, {
+        'salesperson': 'Mary',
+        'sales': 29
+      }];
+      var graphicContainer = document.querySelector('#graphic');
+      var padding = {
+        top: 60,
+        right: 40,
+        bottom: 80,
+        left: 100
+      };
+      var width = graphicContainer.offsetWidth - padding.left - padding.right;
+      var height = this.settings.graphicHeight - padding.top - padding.bottom;
+      var barHeight = 5;
+      var barMarginTop = 8;
+      var y = d3.scaleBand().range([height, 0]);
+      var x = d3.scaleLinear().range([0, width]);
+      var svg = d3.select('#graphic').append('svg').attr('class', 'horizontal-bar').attr('width', width + padding.left + padding.right).attr('height', height + padding.top + padding.bottom).append('g').attr('transform', 'translate(' + padding.left + ',' + padding.top + ')'); // format the data
+
+      data.forEach(function (d) {
+        d.sales = +d.sales;
+      });
+
+      var compare = function compare(a, b) {
+        return b.sales - a.sales;
+      };
+
+      data = data.sort(compare);
+      var maxValue = d3.max(data, function (d) {
+        return d.sales;
+      }); // Scale the range of the data in the domains
+
+      x.domain([0, maxValue + maxValue * .05]);
+      y.domain(data.map(function (d) {
+        return d.salesperson;
+      }));
+      svg.selectAll('.bar').data(data).enter().append('rect').attr('class', 'bar').attr('width', function (d) {
+        return x(d.sales);
+      }).attr('y', function (d) {
+        return y(d.salesperson) + (y.bandwidth() / 2 - barHeight / 2);
+      }).attr('height', barHeight);
+      svg.append('g').attr('transform', 'translate(0,' + (height + 6) + ')').call(d3.axisBottom(x));
+      svg.append('g').call(d3.axisLeft(y).tickSize(0)); // Add graph title
+
+      var title = svg.append('text').attr('class', 'title').text('Title Here');
+      var textWidth = title.node().getBBox().width;
+      var textHeight = title.node().getBBox().height;
+      title.attr('transform', 'translate(' + (width / 2 - textWidth / 2 - padding.left / 2) + ', ' + (-1 * (padding.top / 2) + 10) + ')');
+      var xAxisHeight = 20;
+      var xAxisLabel = svg.append('text').attr('class', 'x-axis-label').text('x-axis label here');
+      textWidth = xAxisLabel.node().getBBox().width;
+      textHeight = xAxisLabel.node().getBBox().height;
+      xAxisLabel.attr('transform', 'translate(' + (width / 2 - textWidth / 2 - padding.left / 2) + ', ' + (height + xAxisHeight + padding.bottom / 2) + ')');
+      var yAxisLabel = svg.append('text').attr('class', 'y-axis-label').text('y-axis label here');
+      textWidth = yAxisLabel.node().getBBox().width;
+      textHeight = yAxisLabel.node().getBBox().height;
+      yAxisLabel.attr('transform', 'translate(' + (-1 * padding.left + textHeight * 2.5) + ', ' + (height / 2 + textWidth / 2) + ') rotate(-90)');
+    }
+  };
+};
+
+},{}],5:[function(require,module,exports){
+"use strict";
+
+var HorizontalBar = require('./components/horizontal-bar.js');
+
 var Graph = require('./components/graph.js');
 
 var Graph2 = require('./components/graph-2.js');
@@ -47,13 +162,14 @@ var Utilities = require('./utils.js');
 
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
+    HorizontalBar().init();
     Graph().init();
     Graph2().init();
     Graph3().init();
   });
 })();
 
-},{"./components/graph-2.js":1,"./components/graph-3.js":2,"./components/graph.js":3,"./utils.js":5}],5:[function(require,module,exports){
+},{"./components/graph-2.js":1,"./components/graph-3.js":2,"./components/graph.js":3,"./components/horizontal-bar.js":4,"./utils.js":6}],6:[function(require,module,exports){
 "use strict";
 
 (function () {
@@ -179,4 +295,4 @@ var Utilities = require('./utils.js');
   module.exports = window.utils;
 })();
 
-},{}]},{},[4]);
+},{}]},{},[5]);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+var HorizontalBar = require('./components/horizontal-bar.js');
 var Graph = require('./components/graph.js');
 var Graph2 = require('./components/graph-2.js');
 var Graph3 = require('./components/graph-3.js');
@@ -6,7 +7,8 @@ var Utilities = require('./utils.js');
 (function () {
 	
 	document.addEventListener('DOMContentLoaded',function(){
-
+		
+		HorizontalBar().init();
 		Graph().init();
 		Graph2().init();
 		Graph3().init();

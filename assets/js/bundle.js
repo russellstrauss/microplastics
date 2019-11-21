06f54caf01e886a95dd86fb346fd814a39d5e2b6
Russell Strauss <russellstrauss@gmail.com>
2019-11-20 19:48:20 -0500

    remove old code

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -491,135 +491,86 @@ module.exports = function () {
       var cupWidth = 250,
           cupHeight = 410;
       var image = svg.append('svg:image').attr('xlink:href', './assets/svg/starbucks.svg').attr('width', cupWidth).attr('height', cupHeight).attr('x', center.x - cupWidth / 2).attr('y', center.y - cupHeight / 2).attr('class', 'cup');
-      self.particles();
       self.useRatio();
-    },
-    particles: function particles() {
-      var colorScale = d3.scaleSequential(d3.interpolateViridis);
-      var network;
-      var particles = [];
-
-      for (var i = 0; i < 1000; i++) {
-        particles.push({
-          x: Math.random() * width,
-          y: Math.random() * height,
-          // x: 0,
-          // y: 0,
-          radius: i % 3 + 2
-        });
-      }
-
-      var nodeG = svg.append('g').attr('class', 'nodes-group');
-      var forceStrength = .1;
-
-      function charge(d) {
-        //console.log(-Math.pow(d.radius, 2.0) * forceStrength)
-        return -Math.pow(d.radius, 2.0) * forceStrength;
-      }
-
-      var simulation = d3.forceSimulation().velocityDecay(0.03) //.force('charge', charge)
-      .force('charge', d3.forceManyBody().strength(-.5)).force('repelForce', d3.forceManyBody().strength(-1).distanceMax(10).distanceMin(5)).force('center', d3.forceCenter(center.x, center.y));
-      var nodeEnter = nodeG.selectAll().data(particles).enter().append('circle').attr('class', 'node').attr('r', function (d) {
-        return d.radius;
-      }).attr('fill', function (d) {
-        return 'rgb(200, 200, 200)';
-      }).attr('stroke', function (d) {
-        return 'rgba(0, 0, 0, .5)';
-      }).attr('opacity', 0).attr('stroke-width', 1);
-
-      var updateParticles = function updateParticles() {
-        nodeEnter = nodeG.selectAll('.node').transition().duration(500) // remove me
-        .attr('opacity', 1).data(particles).merge(nodeEnter);
-        nodeEnter.exit().transition().remove(); // Update and restart the simulation.
-
-        simulation.nodes(nodeEnter);
-        simulation.alpha(1).restart();
-      };
-
-      svg.on('click', function () {
-        simulation.force('repelForce', d3.forceManyBody().strength(-1).distanceMax(2).distanceMin(1));
-        particles = [];
-
-        for (var _i = 0; _i < 1000; _i++) {
-          particles.push({
-            x: Math.random() * width,
-            y: Math.random() * height,
-            radius: _i % 3 + 2
-          });
-        }
-
-        updateParticles();
-        svg.select('.cup').attr('opacity', 0);
-      });
-      simulation.nodes(particles).on('tick', tickSimulation);
-      var done = false;
-
-      function tickSimulation() {
-        nodeEnter.attr('cx', function (d) {
-          return d.x;
-        }).attr('cy', function (d) {
-          return d.y;
-        });
-      }
     },
     longevityTimescale: function longevityTimescale() {
       var self = this;
+      var generationLength = 76;
+      var ratio, remainder;
+      var glyph = document.querySelector('.generation-glyphs .frame');
       var data = [{
         'category': '',
-        'years': 450
+        'years': 425
       }];
-      var graphs = document.querySelectorAll('.longevity');
-      graphs.forEach(function (graph) {
-        var graphicContainer = graph.parentElement;
-        var padding = {
-          top: 5,
-          right: 50,
-          bottom: 80,
-          left: 100
-        };
-        var width = graphicContainer.offsetWidth - padding.left - padding.right;
-        var height = timescaleHeight - padding.top - padding.bottom;
-        var barHeight = 15;
-        var y = d3.scaleBand().range([height, 0]);
-        var x = d3.scaleLinear().range([0, width]);
-        var svg = d3.select(graph).append('svg').attr('width', width + padding.left + padding.right).attr('height', height + padding.top + padding.bottom).append('g').attr('transform', 'translate(' + padding.left + ',' + padding.top + ')'); // format the data
+      var graph = document.querySelector('.longevity');
+      var graphicContainer = graph.parentElement;
+      var padding = {
+        top: 5,
+        right: 50,
+        bottom: 80,
+        left: 100
+      };
+      var width = graphicContainer.offsetWidth - padding.left - padding.right;
+      var height = timescaleHeight - padding.top - padding.bottom;
+      var barHeight = 15;
+      var barWidth = 0;
+      var y = d3.scaleBand().range([height, 0]);
+      var x = d3.scaleLinear().range([0, width]);
+      var svg = d3.select(graph).append('svg').attr('width', width + padding.left + padding.right).attr('height', height + padding.top + padding.bottom).append('g').attr('transform', 'translate(' + padding.left + ',' + padding.top + ')'); // format the data
 
-        data.forEach(function (d) {
-          d.years = +d.years;
-        });
-
-        var compare = function compare(a, b) {
-          return b.years - a.years;
-        };
-
-        data = data.sort(compare);
-        var maxValue = d3.max(data, function (d) {
-          return d.years;
-        }); // Scale the range of the data in the domains
-
-        x.domain([0, maxValue + maxValue * .2]);
-        y.domain(data.map(function (d) {
-          return d.category;
-        }));
-        var xAxisHeight = 20;
-        var xAxisLabel = svg.append('text').attr('class', 'x-axis-label').html('Years to Break Down');
-        var textWidth = xAxisLabel.node().getBBox().width;
-        var textHeight = xAxisLabel.node().getBBox().height;
-        xAxisLabel.attr('transform', 'translate(' + (width / 2 - textWidth) + ', ' + (height + xAxisHeight + padding.bottom / 2) + ')');
-        svg.selectAll('.bar').data(data).enter().append('rect').attr('class', 'bar').attr('width', function (d) {
-          return x(d.years);
-        }).attr('y', function (d) {
-          return y(d.category) + (y.bandwidth() / 2 - barHeight / 2);
-        }).attr('height', barHeight);
-        svg.append('g').attr('transform', 'translate(0,' + (height + 6) + ')').call(d3.axisBottom(x));
-        svg.append('g').call(d3.axisLeft(y).tickSize(0));
+      data.forEach(function (d) {
+        d.years = +d.years;
       });
+
+      var compare = function compare(a, b) {
+        return b.years - a.years;
+      };
+
+      data = data.sort(compare);
+      var maxValue = d3.max(data, function (d) {
+        return d.years;
+      }); // Scale the range of the data in the domains
+
+      x.domain([0, maxValue + maxValue * .2]);
+      y.domain(data.map(function (d) {
+        return d.category;
+      }));
+      var xAxisHeight = 20;
+      var xAxisLabel = svg.append('text').attr('class', 'x-axis-label').html('Years to Break Down');
+      var textWidth = xAxisLabel.node().getBBox().width;
+      var textHeight = xAxisLabel.node().getBBox().height;
+      xAxisLabel.attr('transform', 'translate(' + (width / 2 - textWidth) + ', ' + (height + xAxisHeight + padding.bottom / 2) + ')');
+      svg.selectAll('.bar').data(data).enter().append('rect').attr('class', 'bar').attr('width', function (d) {
+        barWidth = x(d.years);
+        ratio = d.years / generationLength;
+        remainder = x(d.years % generationLength);
+        return x(d.years);
+      }).attr('y', function (d) {
+        return y(d.category) + (y.bandwidth() / 2 - barHeight / 2);
+      }).attr('height', barHeight);
+      var generations = document.querySelector('.generation-glyphs');
+
+      for (var i = 0; i < Math.ceil(ratio) - 1; i++) {
+        generations.append(glyph.cloneNode(true));
+      }
+
+      var graphicWidth = barWidth / ratio;
+      var frames = generations.querySelectorAll('.frame');
+      frames.forEach(function (frame) {
+        var image = frame.querySelector('img');
+        frame.style.width = graphicWidth + 'px';
+        image.width = graphicWidth;
+      });
+      frames[frames.length - 1].style.width = remainder - 5 + 'px';
+      svg.append('g').attr('transform', 'translate(0,' + (height + 6) + ')').call(d3.axisBottom(x));
+      svg.append('g').call(d3.axisLeft(y).tickSize(0));
     },
     useRatio: function useRatio() {
-      var useTimeHours = 4;
+      var useTimeHours = 3;
       var decomposeYears = 450;
       var decomposeHours = decomposeYears * 8760;
       var ratio = decomposeHours / useTimeHours;
+      var totalCount = 0;
       var width;
       var element = document.querySelector('.use-ratio .canvas-holder');
       var message = element.querySelector('.message');
@@ -634,7 +585,7 @@ module.exports = function () {
         element: element,
         handler: function handler(direction) {
           if (direction === 'down') {
-            message.style.marginBottom = '6px';
+            message.style.marginBottom = '4px';
           } else {
             message.style.marginBottom = '-40px';
           }
@@ -658,6 +609,7 @@ module.exports = function () {
 
       function drawDots() {
         var count = 0;
+        var millionCount = 1;
 
         for (var x = dotRadius * 2; x < vw; x += cellSize) {
           for (var y = dotRadius * 2; y < vh; y += cellSize) {
@@ -668,6 +620,13 @@ module.exports = function () {
             context.strokeStyle = 'black';
             context.lineWidth = 1; //context.stroke();
 
+            if (count === 1000000 * millionCount) {
+              var result = millionCount + ' millionX longer than you used it';
+              element.append(result);
+              element.append('test string lkj;lkjdfas;lkj');
+              millionCount++;
+            }
+
             count++;
           }
         }
@@ -676,12 +635,17 @@ module.exports = function () {
       }
 
       var canvasCopies = Math.floor(ratio / countPerCanvas);
+      console.log('Number of canvases: ', canvasCopies);
 
       for (var i = 0; i < canvasCopies + 1; i++) {
         // duplicate multiple copies of the canvas to avoid millions of loops
         element.append(cloneCanvas(canvas));
         canvas.remove();
+        totalCount += countPerCanvas;
+        if (totalCount > 1000000) element.append('1 million times as long as you used it');
       }
+
+      console.log('Total count: ', totalCount);
 
       function cloneCanvas(oldCanvas) {
         var newCanvas = document.createElement('canvas');

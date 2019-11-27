5f1c629c8b74a33f93c43d9514956046d2058164
hyun <htpark@gatech.edu>
2019-11-28 04:30:12 +0900

    test
    d

--- assets/js/components/cumulative-plastics.js
+++ assets/js/components/cumulative-plastics.js
@@ -37,83 +37,82 @@ module.exports = function () {
 			.style('font-size', '1.5em')
 
 /////////////////////////////////////////////
-
 var dataset;
-
+  
 var formatDateIntoYear = d3.timeFormat("%Y");
 var formatDate = d3.timeFormat("%b %Y");
 var parseDate = d3.timeParse("%m/%d/%y");
 
 var startDate = new Date("2004-11-01"),
-    endDate = new Date("2017-04-01");
+	endDate = new Date("2017-04-01");
 
 var margin = {top:0, right:50, bottom:0, left:50},
-    width = 960 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+	width = 960 - margin.left - margin.right,
+	height = 200 - margin.top - margin.bottom;
 
 ////////// slider //////////
 
 var svgSlider = d3.select("#slider")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height);
-    
+	.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height);
+	
 var x = d3.scaleTime()
-    .domain([startDate, endDate])
-    .range([0, width])
-    .clamp(true);
+	.domain([startDate, endDate])
+	.range([0, width])
+	.clamp(true);
 
 var slider = svgSlider.append("g")
-    .attr("class", "slider")
-    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+	.attr("class", "slider")
+	.attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
 
 slider.append("line")
-    .attr("class", "track")
-    .attr("x1", x.range()[0])
-    .attr("x2", x.range()[1])
+	.attr("class", "track")
+	.attr("x1", x.range()[0])
+	.attr("x2", x.range()[1])
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-inset")
+	.attr("class", "track-inset")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-        .on("start.interrupt", function() { slider.interrupt(); })
-        .on("start drag", function() { update(x.invert(d3.event.x)); }));
+	.attr("class", "track-overlay")
+	.call(d3.drag()
+		.on("start.interrupt", function() { slider.interrupt(); })
+		.on("start drag", function() { update(x.invert(d3.event.x)); }));
 
 slider.insert("g", ".track-overlay")
-    .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
+	.attr("class", "ticks")
+	.attr("transform", "translate(0," + 18 + ")")
   .selectAll("text")
-    .data(x.ticks(10))
-    .enter()
-    .append("text")
-    .attr("x", x)
-    .attr("y", 10)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatDateIntoYear(d); });
+	.data(x.ticks(10))
+	.enter()
+	.append("text")
+	.attr("x", x)
+	.attr("y", 10)
+	.attr("text-anchor", "middle")
+	.text(function(d) { return formatDateIntoYear(d); });
 
 var handle = slider.insert("circle", ".track-overlay")
-    .attr("class", "handle")
-    .attr("r", 9);
+	.attr("class", "handle")
+	.attr("r", 9);
 
 var label = slider.append("text")  
-    .attr("class", "label")
-    .attr("text-anchor", "middle")
-    .text(formatDate(startDate))
-    .attr("transform", "translate(0," + (-25) + ")")
+	.attr("class", "label")
+	.attr("text-anchor", "middle")
+	.text(formatDate(startDate))
+	.attr("transform", "translate(0," + (-25) + ")")
 
 ////////// plot //////////
 
 var svgPlot = d3.select("#vis")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height);
+	.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height);
 
 var plot = svgPlot.append("g")
-    .attr("class", "plot")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+	.attr("class", "plot")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
 
-d3.csv("circles.csv", prepare, function(data) {
+d3.csv("./assets/js/data/circles.csv", prepare, function(data) {
   dataset = data;
   drawPlot(dataset);
 })
@@ -126,93 +125,44 @@ function prepare(d) {
 
 function drawPlot(data) {
   var locations = plot.selectAll(".location")
-    .data(data);
+	.data(data);
 
   // if filtered dataset has more circles than already existing, transition new ones in
   locations.enter()
-    .append("circle")
-    .attr("class", "location")
-    .attr("cx", function(d) { return x(d.date); })
-    .attr("cy", height/2)
-    .style("fill", function(d) { return d3.hsl(d.date/1000000000, 0.8, 0.8)})
-    .style("stroke", function(d) { return d3.hsl(d.date/1000000000, 0.7, 0.7)})
-    .style("opacity", 0.5)
-    .attr("r", 8)
-      .transition()
-      .duration(400)
-      .attr("r", 25)
-        .transition()
-        .attr("r", 8);
+	.append("circle")
+	.attr("class", "location")
+	.attr("cx", function(d) { return x(d.date); })
+	.attr("cy", height/2)
+	.style("fill", function(d) { return d3.hsl(d.date/1000000000, 0.8, 0.8)})
+	.style("stroke", function(d) { return d3.hsl(d.date/1000000000, 0.7, 0.7)})
+	.style("opacity", 0.5)
+	.attr("r", 8)
+	  .transition()
+	  .duration(400)
+	  .attr("r", 25)
+		.transition()
+		.attr("r", 8);
 
   // if filtered dataset has less circles than already existing, remove excess
   locations.exit()
-    .remove();
+	.remove();
 }
 
 function update(h) {
   // update position and text of label according to slider scale
   handle.attr("cx", x(h));
   label
-    .attr("x", x(h))
-    .text(formatDate(h));
+	.attr("x", x(h))
+	.text(formatDate(h));
 
   // filter data set and redraw plot
   var newData = dataset.filter(function(d) {
-    return d.date < h;
+	return d.date < h;
   })
   drawPlot(newData);
 }
-// var svg = d3.select(".slider"),
-//     margin = {right: 50, left: 50},
-//     width = +svg.attr("width") - margin.left - margin.right,
-//     height = +svg.attr("height");
 
-// var x = d3.scaleLinear()
-//     .domain([0, 180])
-//     .range([0, width])
-//     .clamp(true);
 
-// var slider = svg.append("g")
-//     .attr("class", "slider")
-//     .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
-
-// slider.append("line")
-//     .attr("class", "track")
-//     .attr("x1", x.range()[0])
-//     .attr("x2", x.range()[1])
-//   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-//     .attr("class", "track-inset")
-//   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-//     .attr("class", "track-overlay")
-//     .call(d3.drag()
-//         .on("start.interrupt", function() { slider.interrupt(); })
-//         .on("start drag", function() { hue(x.invert(d3.event.x)); }));
-
-// slider.insert("g", ".track-overlay")
-//     .attr("class", "ticks")
-//     .attr("transform", "translate(0," + 18 + ")")
-//   .selectAll("text")
-//   .data(x.ticks(10))
-//   .enter().append("text")
-//     .attr("x", x)
-//     .attr("text-anchor", "middle")
-//     .text(function(d) { return d + "°"; });
-
-// var handle = slider.insert("circle", ".track-overlay")
-//     .attr("class", "handle")
-//     .attr("r", 9);
-
-// slider.transition() // Gratuitous intro!
-//     .duration(750)
-//     .tween("hue", function() {
-//       var i = d3.interpolate(0, 70);
-//       return function(t) { hue(i(t)); };
-//     });
-
-// function hue(h) {
-//   handle.attr("cx", x(h));
-//   svg.style("background-color", d3.hsl(h, 0.8, 0.8));
-// }
 		}
 	}
 }

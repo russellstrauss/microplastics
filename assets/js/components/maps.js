cd0c7c5c513e388b3149bfb63d39d212f088c061
Russell Strauss <russellstrauss@gmail.com>
2019-12-03 00:05:57 -0500

    Add graph to geo vis

--- assets/js/components/maps.js
+++ assets/js/components/maps.js
@@ -6,7 +6,7 @@ module.exports = function() {
 	var containerHeight = parseInt(document.querySelector('.fullscreen-map').offsetHeight);
 	var mapWithLabels = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png?access_token={accessToken}';
 	var mapWithoutLabels = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png?access_token={accessToken}';
-	var exportsData, geojson, toolTip;
+	var exportsData, geojson, toolTip, barData;
 	
 	var china = {
 		location: new L.LatLng(23.638, 120.998),
@@ -15,7 +15,7 @@ module.exports = function() {
 	
 	var center = {
 		location: new L.LatLng(30, 20),
-		zoom: 2.5
+		zoom: 1.5
 	}
 	
 	var setLocation = center;
@@ -28,6 +28,10 @@ module.exports = function() {
 	svgLayer.addTo(map);
 	
 	return {
+		
+		settings: {
+			barHeight: 400
+		},
 		
 		init: function() {
 
@@ -79,8 +83,10 @@ module.exports = function() {
 				
 				d3.csv("./assets/js/data/exports.csv", prepare).then(function(data) {
 					exportsData = data;
+					console.log(exportsData);
 					
 					self.showCountries();
+					self.addBarGraph();
 				});
 				
 				function prepare(d) {
@@ -114,7 +120,8 @@ module.exports = function() {
 				accessToken: 'pk.eyJ1IjoiamFnb2R3aW4iLCJhIjoiY2lnOGQxaDhiMDZzMXZkbHYzZmN4ZzdsYiJ9.Uwh_L37P-qUoeC-MBSDteA',
 				edgeBufferTiles: 2,
 				reuseTiles: true,
-				format: 'jpg70'
+				format: 'jpg70',
+				noWrap: true
 			}).addTo(map);
 			
 			map.dragging.disable();
@@ -146,6 +153,7 @@ module.exports = function() {
 			};
 			
 			exportsData = exportsData.sort(sortAmountDesc).slice(0, 20);
+			barData = exportsData.sort(sortAmountDesc).slice(0, 20);
 			//console.log(exportsData);
 			
 			let min = exportsData[19].amount;
@@ -182,6 +190,85 @@ module.exports = function() {
 			
 			var countriesLayer = L.geoJson(geojson, {style: style});
 			countriesLayer.addTo(map);
+		},
+		
+		addBarGraph: function () {
+			
+			let self = this;
+				
+			let graph = document.querySelector('.geo-vis .bar-graph');
+			
+			let graphicContainer = graph.parentElement;
+			var padding = {
+				top: 60,
+				right: 100,
+				bottom: 80,
+				left: 200
+			};
+			
+			var width = graphicContainer.offsetWidth - padding.left - padding.right;
+			var height = self.settings.barHeight - padding.top - padding.bottom;
+			var barHeight = 5;
+			
+			let maxValue = d3.max(exportsData, function (d) {
+				return d.amount;
+			});
+			
+			let compare = function(a, b) {
+				return a.amount - b.amount;
+			};
+			
+			exportsData = exportsData.sort(compare);
+
+			let count = 21;
+			var y = d3.scaleBand().domain(exportsData.map(function (d) {
+				return d.country;
+			})).range([height, 0]);
+			var x = d3.scaleLinear().domain([0, maxValue]).range([0, width - 100]);
+
+			var svg = d3.select(graph).append('svg')
+			.attr('width', width + padding.left + padding.right)
+			.attr('height', height + padding.top + padding.bottom)
+			.append('g')
+			.attr('transform', 'translate(' + padding.left + ',' + padding.top + ')');
+
+			svg.selectAll('.bar')
+				.data(exportsData)
+				.enter().append('rect')
+				.attr('class', 'bar')
+				.attr('width', function(d) {
+					return x(d.amount);
+				})
+				.attr('y', function (d) {
+					return y(d.country) + (y.bandwidth() / 2 - barHeight / 2);
+				})
+				.attr('height', barHeight);
+
+			svg.append('g').attr('transform', 'translate(0,' + (height + 6) + ')').call(d3.axisBottom(x));
+			svg.append('g').call(d3.axisLeft(y).tickSize(0));
+			
+			// Add graph title
+			// let title = svg.append('text') 
+			// 	.attr('class', 'title')
+			// 	.text('Top 20 Global Rivers Ranked by Ocean Plastic Input');
+			// let textWidth = title.node().getBBox().width;
+			// let textHeight = title.node().getBBox().height;
+			// title.attr('transform','translate(' + (width/2 - (textWidth/2) - (padding.left/2)) + ', ' + (-1 * (padding.top/2) + 10) + ')');
+			
+			let xAxisHeight = 20;
+			let xAxisLabel = svg.append('text') 
+				.attr('class', 'x-axis-label')
+				.html('Top 20 Global Plastic Exporters ($USD)');
+			let textWidth = xAxisLabel.node().getBBox().width;
+			let textHeight = xAxisLabel.node().getBBox().height;
+			xAxisLabel.attr('transform','translate(' + (width/2 - (textWidth/2) - (padding.left/2)) + ', ' + (height + xAxisHeight + (padding.bottom/2)) + ')');
+		
+			// let yAxisLabel = svg.append('text') 
+			// 	.attr('class', 'y-axis-label')
+			// 	.text('y-axis label here');
+			// textWidth = yAxisLabel.node().getBBox().width;
+			// textHeight = yAxisLabel.node().getBBox().height;
+			// yAxisLabel.attr('transform','translate(' + (-1 * padding.left + textHeight * 2.5) + ', ' + (height/2 + (textWidth/2)) + ') rotate(-90)');
 		}
 	}
 }

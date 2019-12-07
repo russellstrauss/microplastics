5f65dea74e024db124f020eee31cccd1c29c650f
Russell Strauss <russellstrauss@gmail.com>
2019-12-07 16:07:34 -0500

    Style unit vis

--- assets/js/components/plastic-longevity.js
+++ assets/js/components/plastic-longevity.js
@@ -183,7 +183,7 @@ module.exports = function () {
 				}
 				else {
 					document.querySelector('.longevity').innerHTML = '';
-					document.querySelector('.generation-glyphs').innerHTML = '<div class="frame"><img src="./assets/svg/generation.svg" alt="generation icon"></div>';
+					document.querySelector('.generation-glyphs').innerHTML = '<div class="frame"><img src="./assets/svg/generation-white.svg" alt="generation icon"></div>';
 					self.longevityTimescale();
 				}
 			});
@@ -258,7 +258,7 @@ module.exports = function () {
 					for (var y = dotRadius * 2; y < vh; y += cellSize) {
 						context.beginPath();
 						context.arc(x-dotRadius/2, y-dotRadius/2, dotRadius, 0, 2 * Math.PI, false);
-						context.fillStyle = 'rgba(204, 204, 204, .7)';
+						context.fillStyle = 'rgba(255, 255, 255, .4)';
 						context.fill();
 						count++;
 					}

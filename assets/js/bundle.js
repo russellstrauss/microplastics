61a8d8db56b723a40182583ddf6dbece43cfbe58
Russell Strauss <russellstrauss@gmail.com>
2019-12-08 19:05:54 -0800

    Adding langauge

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -800,7 +800,7 @@ module.exports = function () {
       var parentElement = document.querySelector('.paracoords');
       var svgWidth = parentElement.offsetWidth * .60,
           // setting width to 60% of parent container for responsiveness. Adjust if necessary.
-      svgHeight = window.innerHeight * .60,
+      svgHeight = window.innerHeight * .70,
           margin = {
         top: 30,
         right: 100,
@@ -908,7 +908,7 @@ module.exports = function () {
         });
         x = d3.scalePoint().domain(dimensions).range([0, width]);
         draw(data);
-        selectors.append('h2').text('Adjust Ranking');
+        selectors.append('h2').text('Adjust Weights');
         var table = selectors.append('table').attr('class', 'slider-table');
         Object.keys(function_keys).forEach(function (d) {
           var tr = table.append('tr');
